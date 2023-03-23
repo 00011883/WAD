a6b9cb4bfb1a0014722e00a4fb1d3469668a2fdb
00011883 <wiut00011883@gmail.com>
2023-03-23 06:10:57 +0500

    feat: response caching

--- GamesStore_11883/src/app/app.component.ts
+++ GamesStore_11883/src/app/app.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'GamesStore_11883';
+  sub!: Subscription;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
-    console.log(this.router.url);
+    this.sub = this.router.events
+      .pipe(filter((rs): rs is NavigationEnd => rs instanceof NavigationEnd))
+      .subscribe((event) => {
+        if (event.id === 1 && event.url === event.urlAfterRedirects) {
+          sessionStorage.clear();
+        }
+      });
   }
 
   get isAuth(): boolean {
     return !this.router.url.includes('auth');
+  }
+
+  ngOnDestroy(): void {
+    this.sub && this.sub.unsubscribe();
   }
 }
