4fb89f349bc3250b087b72542dc04fb94dc7a3eb
00011883 <wiut00011883@gmail.com>
2023-03-23 06:09:47 +0500

    refactor: games carousel

--- GamesStore_11883/src/app/components/games/games.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-games',
-  templateUrl: './games.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class GamesComponent {}
